f08d02168fbbc7f0249e68a5573bee2e9a876277
root <root@0b182e965536>
2020-12-05 19:30:22 +0000

    Lots of work done.

--- src/_04_tea_maker/TeaRunner.java
+++ src/_04_tea_maker/TeaRunner.java
@@ -4,7 +4,12 @@ public class TeaRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TeaBag tea = new TeaBag("Mint");
+		Kettle kettle = new Kettle();
+		kettle.Water water = new Water();
+		kettle.boil();
+		kettle.getWater()
+		Cup cup = new Cup();
 	}
 
 }
